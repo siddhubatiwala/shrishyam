48f8d4f99c039abb1fe901035dd3c3a11301a79a
siddhubatiwala <131663859+siddhubatiwala@users.noreply.github.com>
2023-05-08 14:57:54 +0530

    Added index.php

---
+++ index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+      
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+   <link rel="stylesheet" href="footer.css">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="regist.css">
+    <script src="register.js"></script>
+   
+    <title>श्री श्याम मित्र मंडल कल्याण समिति</title>
+</head>
+<!-- <body  onload="displayData()"> -->
+    <body>
+    <section class="header">
+        <nav>
+            <a href="#"><img src="kalash.png" alt="" style="width:100px; height:100px;"></a>
+            <div class="nav-links" id="navLinks">
+                <i class="fa fa-times" onclick="hidemenu()"></i>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#Benifits">Benefits</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#condition">Terms & Conditions</a></li>
+                </ul>
+            </div>
+            <i class="fa fa-bars" onclick="showmenu()"></i>
+        </nav>
+     <div class="text-box">
+        <h1>श्री श्याम मित्र मंडल कल्याण समिति (रजि.)</h1>
+        <p style="font-size: 30px;"><mark>रजि.न. 64648 / 09</mark></p>
+       
+    </div>
+
+
+    
+<!-- cards -->
+
+<section class="course" id="course">
+    
+    <div class="row">
+        <div class="course-col">
+            <h3>महात्वपूर्ण बातें |</h3>
+            <p>1.गरीब कन्याओं का सामूहिक विवाह करवाना |</p>
+            <p>2.गोशाला का निर्माण करना व  चारा व्यवस्था |</p>
+            <p>3.प्रकृति द्वारा अक्षमत आपदायीन होने पर राहत कार्यों में सेवा करना |</p>
+            <p>4. आशिक्षित बच्चे व बुज़ुर्गों को शिक्षा के प्रति जागृत करना व उनकी मदद करना |</p>
+        </div>
+
+        <div class="course-col">
+            <h3>महात्वपूर्ण बातें |</h3>
+            <p>1.बेरोजगार को रोजगार दिलवाना या रोजगार की व्यवस्था करना।</p>
+            <p>2.सार्दियों में निर्धन व आसाय लोगो को कंबल व रजाई की मदद करना |</p>
+            <p>3.धार्मिक स्थान पर निशुल्क या सामान्य मूल्य पर बस व रेलगढ़ी द्वारा यात्रा पर ले जाना।</p>
+            <p>4. किसी व्यक्ति के द्वारा किया गया अत्याचार या उसको सताना या किसी संगठन पे होना वजह होना चाहिए, सभी घाटों पर समिति का पूर्ण सहयोग व कानून कार्यवाही होगी|</p>
+         </div>
+
+        <!-- <div class="course-col">
+            <h3>Post Graduation</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quod maxime, eos dolor modi tempore minima culpa 
+                voluptates aperiam obcaecati, saepe quidem facilis sequi eveniet sed possimus eaque. Tenetur, repellendus.</p>
+        </div> -->
+    </div>
+</section>
+ </section>  
+ <!-- About us  -->
+ <div class="about" id="about" >
+    <h1 style="color:black;">  मैं समिति द्वारा निर्धारित नियम एवं शर्तों का पालन करूंगा/करूंगी! </h1>
+    <h3 style="font-size:30px" >समिति की प्रमुख व आगामि गतिविधियां</h3>
+                
+        <ul>
+            <li><span>1</span>. धर्म व आध्यात्मिक ज्ञान के लिए मासिक व वार्षिक संकीर्तनो का आयोजन |</li>
+            <li><span>2</span>. गरीब कन्याओं का सामूहिक विवाह करवाना |</li>
+            <li><span>3</span>. सभी बीमारियों का योग और प्राकृतिक चिकित्सा के  द्वारा उपचार  कारवाना |</li>
+            <li><span>4</span>. आशिक्षित बच्चे व बुज़ुर्गों को शिक्षा के प्रति जागृत करना व उनकी मदद करना |</li>
+            <li><span>5</span>.सार्दियों में निर्धन व आसाय लोगो को कंबल व रजाई की मदद करना |</li>
+            <li><span>6</span>.प्रकृति द्वारा अक्षमत आपदायीन होने पर राहत कार्यों में सेवा करना |</li>
+            <li><span>7</span>.आशाय व निर्धन लोगो के लिए वकीलो द्वारा मुफ्त कानूनी सलाह दिलवाना |</li>
+            <li><span>8</span>.गोशाला का निर्माण करना व चारा व्यवस्था |</li>      
+        </ul> 
+ </div>
+
+
+ <!-- condition  -->
+ <div class="condition"  id="condition" >
+    <h1 style="color:black;"> नियम व शर्ते  </h1>
+        <ul>
+                    <li><span>1</span>. सदस्य को सलाना रु 3100/--- या महीने के रु 300/--- नगदी या मंडल खाते में जमा करना रहना होगा |</li>
+                    <li><span>2</span>. सदस्य को समिति द्वारा आयोगित समस्त धार्मिक सामाजिक गतिविधियो में अपना पूर्ण सहयोग देना होगा अपनी उपाष्ठिती करनी होगी |</li>
+                    <li><span>3</span>. समति की प्रत्येक बैठक में समस्त सदास्यों को भाग लेना होगा बैठक अलग अलग स्थानों पर होगी। </li>
+                    <li><span>4</span>. समिति के सभी सदस्य अन्य लोगो लो मंडल (समिति) में शामिल करके समिति के विकास में अपना सहयोग देना होगा।</li>
+                    <li><span>5</span>. अपने सुझावों व शिकायतों को उचित तारिको से समिति के पदाधिकारियो तक ले जाए।</li>
+                    <li><span>6</span>. समिति के नियम और निर्णय के विरुद्ध आचारण करने वाले  सदस्य की सदस्यता खत्म कर दी जाएगी | </li>
+                    <li><span>7</span>. समिति से अलग होने के लिए कार्यालय में लिख पत्र जमा कारवाना आवश्यक है</li>
+                    <li><span>8</span>. समिति के पुराने संरक्षक सदसे जो अर्थिक आ रहे हैं.इच्छा अनुसर उनका सहयोग हमेशा की तरह मिलता रहेगा |</li> 
+        </ul>
+    </div>
+</div> 
+<!-- benefits -->
+<div class="condition"  id="benefits" >
+    <h1 style="color:black;"> समिति से जुड़ने के फायदे </h1>
+        <ul>
+                    <li><span>1</span>. संस्था के द्वारा सदसयों के निवास स्थानों पर प्रत्येक माह किसी एक सदस्य के यहां निशुल्क मासिक संकीर्तन का नियोजन किया जाएगा|</li>
+                    <li><span>2</span>.धार्मिक स्थान पर निशुल्क या सामान्य मूल्य पर बस व रेलगढ़ी द्वारा यात्रा पर ले जाना।</li>
+                    <li><span>3</span>. पुरानी बीमारी का मुफ्त में योग और प्राकृतिक चिकित्सा के द्वारा उपचार करावाना।</li>
+                    <li><span>4</span>.सदस्य के बीमार होने पर या कोई घटाना होने पर मुफ्त में उनकी चिकित्सा करावाना।</li>
+                    <li><span>5</span>. किसी व्यक्ति के द्वारा किया गया अत्याचार या उसको सताना या किसी संगठन पे होना वजह होना चाहिए, सभी घाटों पर समिति का पूर्ण सहयोग व कानून कार्यवाही होगी|</li>
+                    <li><span>6</span>. बेरोजगार को रोजगार दिलवाना या रोजगार की व्यवस्था करना। </li>
+                    <li><span>7</span>. समिति द्वारा किया गया वार्षिक महोत्सव जिस्मे भाग लेने पर :-|</li>
+                    <li><span>(क)</span>. व्यक्तियो के हाव भाव में परिवर्तन। </li> 
+                    <li><span>(ख)</span>. मानसिक तनाव में कमी व आप व्यवहार में जागरुकता तथा नई गतिविधियों का निर्माण। </li>
+                    <li><span>8</span>. एक विशाल संगठन का साथ।</li>
+                    
+        </ul>
+    </div>
+</div>  
+
+
+<!-- Membership Form  -->
+
+<div class="container" id="contact">
+    <h1 style="text-align:center;">सदस्यता पंजिकरण।</h1>
+    <form method="post" onsubmit="return validateform()">
+      <div class="form-group">
+        <label for="firstname">आपका नाम</label>
+        <input type="text" id="name" name="" placeholder="Enter your first name">
+        <span id="nameError" class="error"></span>
+      </div>
+
+      <div class="form-group">
+        <label for="lastname">पिता का नाम</label>
+        <input type="text" id="fathername" name="" placeholder="Enter your last name">
+        <span id="fathernameError" class="error"></span>
+      </div>
+     
+      <div class="form-group">
+        <label for="birthdate">आपकी जन्मतिथि </label>
+        <input type="date" id="birthdate" name="">
+        <span id="birthError" class="error"></span>
+      </div>
+      <div class="form-group">
+        <label for="address">आपका पता</label>
+        <textarea id="address" name="" placeholder="Enter your address"></textarea>
+        <span id="addressError" class="error"></span>
+      </div>
+      <div class="form-group">
+        <label for="phone">आपका फोन नंबर</label>
+        <input type="tel" id="phone" name="" placeholder="Enter your phone number">
+        <span id="phoneError" class="error"></span>
+      </div>
+      <div class="form-group">
+        <label for="id">व्यवसाय</label>
+        <input type="text" id="occupation" name="">
+        <span id="occupationError" class="error"></span>
+        
+      </div>
+      <div class="form-group">
+        <label for="id">ब्लड ग्रुप</label>
+        <input type="text" id="blood" name="" >
+        <span id="bloodError" class="error"></span>
+      </div>
+
+      <div class="form-group">
+        <label for="id">फोटो</label>
+        <input type="file" id="photo" name="" >
+        <span id="photoError" class="error"></span>
+      </div>
+      
+      <div class="form-group">
+        <div class="form-check">
+          <input type="checkbox" id="agree" name="agree" class="form-check-input">
+          <label for="agree" class="form-check-label">मैं नियमों और शर्तों से सहमत हूं।</label>
+          <span id="checkError" class="error"></span>
+        </div>
+      </div>
+      <input type="submit" value="पंजिकरण।" class="btn">
+    </form>  
+   </div>
+<!-- Footer section  -->
+<section class="footer">
+    <div class="add" style="text-align:center;">
+        <h2>कार्यालय</h2>
+        <p>ऑफिस नं0 3, CRL, चौ. चरण सिंह मार्ग मोलारबंद बदरपुर, नई दिल्ली-110044</p>
+        <h2>संपर्क</h2>
+        <p>मोबाइल नंबर :9999678799,  9810342099,  9891100002</p>
+    </div>
+    <div class="social">
+        <a href="#"><i class="fa fa-facebook"></i></a>
+        <a href="https://www.instagram.com/shadvalpay/?igshid=ZGUzMzM3NWJiOQ%3D%3D"><i class="fa fa-instagram"></i></a>
+        <a href="https://www.youtube.com/results?search_query=shadvalpay"><i class="fa fa-youtube"></i></a>
+        <a href="#"><i class="fa fa-twitter"></i></a>
+        <a href="#"><i class="fa fa-linkedin"></i></a>
+    </div>
+
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#benefits">Benefits</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#condition">Terms & Coditions</a></li>
+    </ul>
+
+    <div class="copyright-text">
+                            <p style="text-align:center;">Copyright &copy; 2023, All Right Reserved <a href="http://shadvalpay.in/">ShadvalPay</a></p>
+                        </div>
+  </section>
+
+
+<!-- Javascript for toggle menu  -->
+    <script>
+        var navLinks=document.getElementById('navLinks');
+        
+        function showmenu(){
+            navLinks.style.right="0";
+        }
+
+        function hidemenu(){
+            navLinks.style.right="-200px";
+        }
+    </script>
+    
+</body>
+</html>
